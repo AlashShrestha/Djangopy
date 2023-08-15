ae003bf3b1b0d222e0618fbe40fe0c1f2927db38
Alash Shrestha <np03cs4s220081@heraldcollege.edu.np>
2023-08-15 09:21:22 +0545

    Static file location

--- Demo/settings.py
+++ Demo/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-STATIC_URL = "public/"
-STATIC_ROOT = os.path.join(BASE_DIR, "public")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "public")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
