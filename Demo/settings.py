5dc37b04554f22529eddc7af318992940391bb37
Alash Shrestha <np03cs4s220081@heraldcollege.edu.np>
2023-08-16 07:40:02 +0545

    Static

--- Demo/settings.py
+++ Demo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["www.alashshrestha.com.np/"]
+ALLOWED_HOSTS = ["www.alashshrestha.com.np/", "blogapp-g4hv.onrender.com/"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
